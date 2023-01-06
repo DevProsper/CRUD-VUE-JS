f8f83745c970c835f9dd68ccf5b6d693e3315125
=DevProsper <=prosperngouari38@gmail.com>
2023-01-06 14:02:29 +0100

    Mise en place du state des menus et organisation de travail

--- js/app.js
+++ js/app.js
@@ -1,4 +1,54 @@
-const App = {}
+const App = {
 
+    data(){
+        return {
+            showHome : false,
+            showCreateForm : false,
+            showStudentsList : false,
+        }
+    },
 
-Vue.createApp(App).mon
+    mounted() {
+        this.showHome = true;
+    },
+
+    methods: {
+
+        goToHome(){
+            this.changeNavigationState("home")
+        },
+
+        gotToCreateForm(){
+            this.changeNavigationState("create")
+        },
+
+        goToStudentsList(){
+            this.changeNavigationState("list")
+        },
+
+        changeNavigationState(destination) {
+            this.showCreateForm = false
+            this.showHome = false
+            this.showStudentsList = false
+
+            switch (destination) {
+                case "home":
+                    this.showHome = true
+                    break;
+                case "create":
+                    this.showCreateForm = true
+                    break;
+                case "list":
+                    this.showStudentsList = true
+                    break;
+
+                default:
+                    this.showHome = true
+                    break;
+            }
+    },
+}
+
+}
+
+Vue.createApp(App).mount("#app")
